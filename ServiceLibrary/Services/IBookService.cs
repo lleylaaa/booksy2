db2b67a7001ee0cfa1b3c658e5c93ec10c47118a
lleylaaa <yusuf@LEYLAS-LAPTOP>
2026-05-30 15:42:00 +0200

    lege class1 en stubs verwijderd

--- ServiceLibrary/Services/IBookService.cs
+++
@@ -1,3 +0,0 @@
-// IBookService is verwijderd conform sprint-feedback.
-// BookService wordt direct via DI geregistreerd zonder interface.
-namespace ServiceLibrary.Services { }
--- ServiceLibrary/Services/IUserService.cs
+++
@@ -1,3 +0,0 @@
-// IUserService is verwijderd conform sprint-feedback.
-// UserService wordt direct via DI geregistreerd zonder interface.
-namespace ServiceLibrary.Services { }
